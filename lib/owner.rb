90786a19344b269f14d1e77387d526364337ff5f
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-14 09:39:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -49,6 +49,10 @@ class Owner
     self.cats.each { |cat| cat.mood = "happy" }
   end
 
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+  
   def sell_pets
     pets = self.dogs + self.cats
 
@@ -56,9 +60,5 @@ class Owner
       pet.mood = "nervous"
       pet.owner = nil
     end
-  end
-
-  def list_pets
-    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
 end
